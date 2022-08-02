e9b212b38bbe32c17fe4d11d931d9fc1377d09ee
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-08-02 04:31:45 -0700

    feat: :sparkles: creating route users, and get all users controller

--- src/controllers/Users.controller.js
+++ src/controllers/Users.controller.js
@@ -1,9 +1,18 @@
-exports.getAllUsersController = (req, res) => {
-  res.status(500).json({
-    status: 500,
-    message: 'this route is not yet defined',
+const UserModel = require('../models/user.model')
+const catchAsync = require('../utils/catchAsync')
+
+exports.getAllUsersController = catchAsync(async (req, res, next) => {
+  const users = await UserModel.find()
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    requestTimeAte: req.requestTimeAt,
+    data: {
+      users,
+    },
   })
-}
+})
 
 exports.createUserController = (req, res) => {
   res.status(500).json({
--- src/routes/tours.routes.js
+++ src/routes/tours.routes.js
@@ -11,10 +11,11 @@ const {
 } = require('../controllers/Tours.controller')
 
 const { aliasTopTours } = require('../middlewares/tours.middlewares')
+const { protect } = require('../middlewares/auth.middlewares')
 
 const router = Router()
 
-router.route('/').get(getAllToursController).post(createTourController)
+router.route('/').get(protect, getAllToursController).post(createTourController)
 router.route('/top-5-cheap').get(aliasTopTours, getAllToursController)
 router.route('/tour-stats').get(getTourStatsController)
 router.route('/month-plan/:year').get(getMonthlyPlan)
