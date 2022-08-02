9da0cafe9529de48bf97bcd3e7756c08741cbb13
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-08-02 04:34:25 -0700

    feat: :sparkles: creating user registration controller, and of  login

--- src/controllers/Auth.controller.js
+++ src/controllers/Auth.controller.js
@@ -1,13 +1,57 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
 const UserModel = require('../models/user.model')
 const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  })
+}
 
 exports.signUpController = catchAsync(async (req, res, next) => {
-  const newUser = await UserModel.create(req.body)
+  const newUser = await UserModel.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
+  })
+
+  let userResult = newUser.toObject()
+  delete userResult['password']
+
+  const token = signToken(userResult._id)
 
   res.status(201).json({
     status: 'success',
     data: {
-      user: newUser,
+      token,
+      user: userResult,
     },
+  })
+})
+
+exports.loginController = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return next(new AppError(`Please provider email and password`, 400))
+  }
+
+  const user = await UserModel.findOne({ email }).select('+password')
+  const passwordIsValid = await user.correctPassword(password, user.password)
+
+  if (!user || !passwordIsValid) {
+    return next(new AppError(`Invalid email or password`, 401))
+  }
+
+  const token = signToken(user._id)
+
+  res.status(200).json({
+    status: 'success',
+    token,
   })
 })
