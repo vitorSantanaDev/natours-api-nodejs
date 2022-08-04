09aeaef6033d3387962bfa45acb9cf13c620b364
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-08-04 05:09:31 -0700

    feat: :sparkles: creating util for sending emails

---
+++ src/utils/email.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer')
+
+const sendEmail = async (options) => {
+  // Create transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  })
+  // Define the email options
+  const mailOptions = {
+    from: process.env.USERNAME_AND_EMAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html: ''
+  }
+  // Actually send the email
+  await transporter.sendMail(mailOptions)
+}
+
+module.exports = sendEmail
