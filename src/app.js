8e181b59390d096b3c404820460edb6750b9649e
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-08-09 05:21:44 -0700

    feat: :lock: adding hpp, middleware to protect against HTTP Parameter Pollution attacks

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const tourRouter = require('./routes/tours.routes')
 const userRouter = require('./routes/users.routes')
@@ -40,6 +41,20 @@ app.use(mongoSanitize())
 
 // Data sanatization against XSS
 app.use(xss())
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+)
 
 // Serving static files
 app.use(express.static(`${__dirname}/../public`))
