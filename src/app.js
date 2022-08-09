9506f9fef8d27f0d40781c546d258ed94dc84a04
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-08-09 04:23:14 -0700

    feat: :sparkles: limiting the number of requests coming from the same IP

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
 
 const tourRouter = require('./routes/tours.routes')
 const userRouter = require('./routes/users.routes')
@@ -8,9 +9,20 @@ const AppError = require('./utils/appError')
 const errorController = require('./controllers/Error.controller')
 
 const app = express()
+
+// Global middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: `Too many requests from this IP, please try again in an hour!`,
+})
+
+app.use('/api', limiter)
+
 app.use(express.json())
 app.use(express.static(`${__dirname}/../public`))
 
