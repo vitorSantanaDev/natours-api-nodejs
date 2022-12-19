c0c925d9f0ad86f8493391321319133303006955
vitorSantanaDev <vitor.santana@onesight.com.br>
2022-12-19 20:46:50 -0300

    feat: :sparkles: configuring email delivery

--- src/controllers/Auth.controller.js
+++ src/controllers/Auth.controller.js
@@ -4,7 +4,7 @@ const crypto = require('crypto')
 const UserModel = require('../models/user.model')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
-const sendEmail = require('../utils/email')
+const Email = require('../utils/email')
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -42,6 +42,10 @@ exports.signUpController = catchAsync(async (req, res, next) => {
     passwordChangedAt: req.body.passwordChangedAt,
     role: req.body.role,
   })
+
+  const url = `${req.protocol}://${req.get('host')}/me`
+
+  await new Email(newUser, url).sendWelcome()
 
   let userResult = newUser.toObject()
   delete userResult['password']
@@ -85,18 +89,13 @@ exports.forgotPasswordController = catchAsync(async (req, res, next) => {
   const resetToken = await user.createPasswordResetToken()
   await user.save({ validateBeforeSave: false })
   // send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/reset-password/${resetToken}`
-
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: `Your passord reset token (valid for 10 min)`,
-      message,
-    })
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/reset-password/${resetToken}`
+
+    await new Email(user, resetURL).sendPasswordReset()
     res.status(200).json({
       status: 'success',
       message: 'Token send to email',
--- src/utils/email.js
+++ src/utils/email.js
@@ -1,25 +1,67 @@
 const nodemailer = require('nodemailer')
+const pug = require('pug')
+const htmlToText = require('html-to-text')
 
-const sendEmail = async (options) => {
-  // Create transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  })
-  // Define the email options
-  const mailOptions = {
-    from: process.env.USERNAME_AND_EMAIL,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html: ''
+module.exports = class EmailHandle {
+  constructor(user, url) {
+    this.to = user.email
+    this.firstName = user.name.split(' ')[0]
+    this.url = url
+    this.from = process.env.USERNAME_AND_EMAIL
   }
-  // Actually send the email
-  await transporter.sendMail(mailOptions)
-}
 
-module.exports = sendEmail
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      })
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    })
+  }
+
+  async send(template, subject) {
+    // 1 - Render HTML based on a pug template
+    const htmlTemplate = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    )
+
+    // 2 - Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      text: htmlToText.htmlToText(htmlTemplate),
+      html: htmlTemplate,
+    }
+
+    // 3 - Create a transport and send email
+    await this.newTransport().sendMail(mailOptions)
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!')
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for 10 minutes)!'
+    )
+  }
+}
